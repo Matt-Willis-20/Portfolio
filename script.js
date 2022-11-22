0c483647814df1954276bb56b1947939eb9ec000
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-11-22 09:20:19 +0000

    feat: add slide function to image carousel
    for home page and about page image slider

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 /* --------  NAVBAR HAMBURGER MENU ----------*/
 
 const hamburger = document.querySelector(".hamburger");
@@ -8,99 +6,97 @@ const navMenu = document.querySelector(".main-nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
-
 
 /* --------------- DARK MODE STYLING --------------- */
 
-const toggleSwitch =
-    document.querySelector('.theme-slider input[type="checkbox"]');
- 
+const toggleSwitch = document.querySelector(
+  '.theme-slider input[type="checkbox"]'
+);
+
 /* Function to change theme */
 function switchTheme(e) {
- 
-    /* Once checkbox is checked default theme change to dark */
-    if (e.target.checked) {
-        document.documentElement.setAttribute('theme', 'dark');
-        localStorage.setItem("data-theme", "dark");
-    }
- 
-    /* While page in dark mode and checkbox is checked then theme back to change light*/
-    else {
-        document.documentElement.setAttribute('theme', 'light');
-        localStorage.setItem("data-theme", "light");
-    }
-}
- 
-toggleSwitch.addEventListener('change', switchTheme, false);
+  /* Once checkbox is checked default theme change to dark */
+  if (e.target.checked) {
+    document.documentElement.setAttribute("theme", "dark");
+    localStorage.setItem("data-theme", "dark");
+  } else {
 
-if(localStorage.getItem("data-theme")) {
-  document.documentElement.setAttribute("theme", localStorage.getItem("data-theme"));
-  if(localStorage.getItem("data-theme") == "dark") {
+  /* While page in dark mode and checkbox is checked then theme back to change light*/
+    document.documentElement.setAttribute("theme", "light");
+    localStorage.setItem("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+if (localStorage.getItem("data-theme")) {
+  document.documentElement.setAttribute(
+    "theme",
+    localStorage.getItem("data-theme")
+  );
+  if (localStorage.getItem("data-theme") == "dark") {
     toggleSwitch.checked = true;
   }
-  } 
-
+}
 
 /*-------- add/remove dark-mode element on scroll ------------*/
 
-const darkModeBar = document.querySelector('.dark-mode')
+const darkModeBar = document.querySelector(".dark-mode");
 let scrollPos = 0;
 
 function hideDarkModeBar() {
-    let windowY = window.scrollY;   // .scrollY position is how far in pixels we are from top of page
-    if (windowY < scrollPos) {
-        //scrolling up
-        darkModeBar.classList.add('is-visible');
-        darkModeBar.classList.remove('is-hidden');
-    } else {
-        // scrolling down
-        darkModeBar.classList.add('is-hidden');
-        darkModeBar.classList.remove('is-visible');
-    };
-    scrollPos = windowY;  
+  let windowY = window.scrollY; // .scrollY position is how far in pixels we are from top of page
+  if (windowY < scrollPos) {
+    //scrolling up
+    darkModeBar.classList.add("is-visible");
+    darkModeBar.classList.remove("is-hidden");
+  } else {
+    // scrolling down
+    darkModeBar.classList.add("is-hidden");
+    darkModeBar.classList.remove("is-visible");
+  }
+  scrollPos = windowY;
 }
 
 // to prevent throttling and not fire off multiple events with every single move
 // we add a debounce function which will make it wait 10ms before firing
 
 function debounce(func, wait = 15, immediate = true) {
-    let timeout;
-    return function() {
-      let context = this, args = arguments;
-      let later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      let callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
+}
 
-window.addEventListener('scroll', debounce(hideDarkModeBar));
-
+window.addEventListener("scroll", debounce(hideDarkModeBar));
 
 
 /* --------  Fade in from bottom on project cards ---------*/
 
-
 // target the elements we want to fade in (we have multiple elements
 // so use querySelectorALL)
 const scrollElements = document.querySelectorAll(".js-scroll");
-
 
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
@@ -131,75 +127,96 @@ const handleScrollAnimation = () => {
     if (elementInView(el, 1.1)) {
       displayScrollElement(el);
     } else if (elementOutofView(el)) {
-      hideScrollElement(el)
+      hideScrollElement(el);
     }
-  })
-}
+  });
+};
 
 //initialize throttleTimer as false
 let throttleTimer = false;
- 
+
 const throttle = (callback, time) => {
-    //don't run the function while throttle timer is true
-    if (throttleTimer) return;
-     
-    //first set throttle timer to true so the function doesn't run
-    throttleTimer = true;
-     
-    setTimeout(() => {
-        //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed 
-        callback();
-        throttleTimer = false;
-    }, time);
-}
+  //don't run the function while throttle timer is true
+  if (throttleTimer) return;
 
+  //first set throttle timer to true so the function doesn't run
+  throttleTimer = true;
 
-window.addEventListener("scroll", () => { 
+  setTimeout(() => {
+    //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed
+    callback();
+    throttleTimer = false;
+  }, time);
+};
+
+window.addEventListener("scroll", () => {
   throttle(handleScrollAnimation, 250);
 });
 
-
-
 /* --------- FILTER PROJECTS BY TECH ---------- */
 
-const divs = document.querySelectorAll('.filterDiv');
-const filterButtons = document.querySelectorAll('.filter-button');
-const filterNumber = document.querySelector('.filtered-number');
+const divs = document.querySelectorAll(".filterDiv");
+const filterButtons = document.querySelectorAll(".filter-button");
+const filterNumber = document.querySelector(".filtered-number");
 
 filterNumber.innerHTML = divs.length;
 
 function filterSelection(e) {
   let targetBtn = e.target.textContent.toLowerCase();
-  targetBtn === 'all' ?
-    divs.forEach(div => {
-      div.classList.contains('hide') ?
-        div.classList.remove('hide') : null
-    }) :
-    divs.forEach(div => {
-      div.classList.contains(targetBtn) ?
-      div.classList.remove('hide') : div.classList.add('hide')      
-      })  
-      
-      let hiddenDivs = document.querySelectorAll('.hide');
-      NumberOfHiddenDivs = hiddenDivs.length;
-      let visibleDivs = divs.length - NumberOfHiddenDivs;
-      filterNumber.innerHTML = visibleDivs;
-    }
+  targetBtn === "all"
+    ? divs.forEach((div) => {
+        div.classList.contains("hide") ? div.classList.remove("hide") : null;
+      })
+    : divs.forEach((div) => {
+        div.classList.contains(targetBtn)
+          ? div.classList.remove("hide")
+          : div.classList.add("hide");
+      });
 
-  function changeColor(e) {
-      filterButtons.forEach(filterButton => {filterButton.classList.remove('active-filter')})
-      e.target.classList.add('active-filter');
-    }
+  let hiddenDivs = document.querySelectorAll(".hide");
+  NumberOfHiddenDivs = hiddenDivs.length;
+  let visibleDivs = divs.length - NumberOfHiddenDivs;
+  filterNumber.innerHTML = visibleDivs;
+}
+
+function changeColor(e) {
+  filterButtons.forEach((filterButton) => {
+    filterButton.classList.remove("active-filter");
+  });
+  e.target.classList.add("active-filter");
+}
+
+filterButtons.forEach((filterButton) =>
+  filterButton.addEventListener("click", filterSelection)
+);
+filterButtons.forEach((filterButton) =>
+  filterButton.addEventListener("click", changeColor)
+);
 
 
-filterButtons.forEach(filterButton => filterButton.addEventListener('click', filterSelection));
-filterButtons.forEach(filterButton => filterButton.addEventListener('click', changeColor));
+// ----------------  IMAGE SLIDER ---------------------------
 
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
 
+nextButton.addEventListener("click", (e) => {
+  // when clicked, get the width of the value of one slide
+  const slideWidth = slide.clientWidth;
+  // increase scrollLeft property by the slidewidth
+  slidesContainer.scrollLeft += slideWidth;
+  console.log("clicked");
+});
 
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+  console.log("clicked");
+});
 
-// footer date
+// footer date ----------------------------------------
 
-const date = document.querySelector('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+const date = document.getElementById("date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
