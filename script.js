f8f9d6740f999950da3a3bbb8cd76449b9443e1b
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-11-22 09:34:19 +0000

    refactor: remove reference to image slider
    imageslider.js has replaced this code

--- script.js
+++ script.js
@@ -194,27 +194,6 @@ filterButtons.forEach((filterButton) =>
 );
 
 
-// ----------------  IMAGE SLIDER ---------------------------
-
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-
-nextButton.addEventListener("click", (e) => {
-  // when clicked, get the width of the value of one slide
-  const slideWidth = slide.clientWidth;
-  // increase scrollLeft property by the slidewidth
-  slidesContainer.scrollLeft += slideWidth;
-  console.log("clicked");
-});
-
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-  console.log("clicked");
-});
-
 // footer date ----------------------------------------
 
 const date = document.getElementById("date");
